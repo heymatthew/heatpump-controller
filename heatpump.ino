b74a25a680c5aebf182e3a970cbdc1be3104757e
Matthew B. Gray <code@matthew.nz>
2015-09-27 15:48:39 +1300

    Works with Mizi heat pump

--- heatpump.ino
+++ heatpump.ino
@@ -13,38 +13,27 @@ IRsend irsend(D3);
 int led = D7;
 int msDelay = 5000;
 
-void setup()
-{
-  Serial.begin(9600);
-  pinMode(led, OUTPUT);
-  Serial.println("Ready and waiting...");
+int run(String command) {
+  int temperature = command.toInt();
+  bool turnOn = (16 < temperature && temperature < 30);
+
+  if (turnOn) {
+    digitalWrite(led, HIGH);
+    irsend.sendHvacMitsubishi(HVAC_HOT, temperature, FAN_SPEED_AUTO, VANNE_AUTO_MOVE, HVAC_ON);
+  }
+  else {
+    digitalWrite(led, LOW);
+    irsend.sendHvacMitsubishi(HVAC_HOT, 22, FAN_SPEED_AUTO, VANNE_AUTO_MOVE, HVAC_OFF);
+  }
+
+  return temperature;
 }
 
-int mode;
-void loop() {
-    if (Serial.available() > 0) {
-        mode = Serial.read();
+void setup() {
+  pinMode(led, OUTPUT);
+  Spark.function("heatpump", run);
+}
 
-        if (mode == NUM_2) {
-            RGB.control(true);
-            RGB.color(255,255,0);
-            delay(1000);
-            Serial.println("Hello world!");
-            delay(1000);
-            RGB.color(0,0,0);
-        } else if (mode == NUM_1) {
-            Serial.println("AC: ON");
-            digitalWrite(led, HIGH);
-            irsend.sendHvacMitsubishi(HVAC_HOT, 20, FAN_SPEED_AUTO, VANNE_AUTO_MOVE, HVAC_ON);
-        } else if (mode == NUM_0) {
-            Serial.println("AC: OFF");
-            digitalWrite(led, LOW);
-            irsend.sendHvacMitsubishi(HVAC_HOT,  20,  FAN_SPEED_AUTO, VANNE_AUTO_MOVE, HVAC_OFF);
-        } else if (mode == ENTER) {
-          // do nothing
-        } else {
-            Serial.print("Unrecognised mode: ");
-            Serial.println(mode, DEC);
-        }
-    }
+void loop() {
+  // Controlled through te cloud
 }
